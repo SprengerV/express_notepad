f948f6bb312f2ca48f8765a7f62c2929babeb18c
SprengerV <b.francis@sprengerv.com>
2021-03-02 16:10:26 -0500

    fixed htmlRoutes export

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
-const apiRoutes = (app) => {
+const htmlRoutes = (app) => {
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-        res.sendFile(path.join(__dirname, '..', 'public', 'assets', 'js', 'index.js'));
     });
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
-        res.sendFile(path.join(__dirname, '..', 'public', 'assets', 'js', 'index.js'));
     });
 };
 
-module.exports = apiRoutes;
+module.exports = htmlRoutes;
